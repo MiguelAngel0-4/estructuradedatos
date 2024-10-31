5dd662ca628c1b34e0e5f76760aa3c217a72971c
Miguel <migue2801224@gmail.com>
2024-10-30 22:58:01 -0500

    segundo comentario

--- 2clase/array.js
+++ 2clase/array.js
@@ -1,3 +1,8 @@
 const array = [1, 2, 3, 4, 5]
 
-console.log(`Accediendo al indice ${array[3]}`)//Acceder a un elemento de Array mediante su índice
+console.log(`Accediendo al indice ${array[3]}`)//Accede a un elemento de Array mediante su índice
+
+console.log('Recorrer un array con for')
+for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+} // El ciclo for es una estructura de control que te permite iterar sobre los elementos de un array de manera manual, controlando el índice y los límites de la iteración.
