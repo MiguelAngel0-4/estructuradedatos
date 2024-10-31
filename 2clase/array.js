5378f0767dbb3cdba4b19397bee5891e3b4f1da6
Miguel <migue2801224@gmail.com>
2024-10-30 23:02:34 -0500

    cuarto comentario

--- 2clase/array.js
+++ 2clase/array.js
@@ -12,3 +12,14 @@ array.forEach((elemento, index) => {
     console.log(`${elemento} en la posición ${index}`)
 }) // forEach no te da ese nivel de control. No puedes usar break o continue para detener o saltar elementos. 
    //Para detener forEach, necesitarías formas alternativas, como devolver false o utilizar try...catch, pero en general, no está diseñado para ese propósito.
+
+//get index
+const array1 = [10, 23, 31, 42, 53]
+
+console.log('Recorrer un array de forEach')
+array1.forEach((elemento, index) => {
+    console.log(`${elemento} en la posición ${index}`)
+})
+
+const index = array1.indexOf(53)//Busca el indice del arreglo
+console.log(index)
