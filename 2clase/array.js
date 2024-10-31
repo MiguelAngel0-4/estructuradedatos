4e63b0d5e518d233399e9796670bb85d138a8270
Miguel <migue2801224@gmail.com>
2024-10-30 22:59:33 -0500

    tercer comentario

--- 2clase/array.js
+++ 2clase/array.js
@@ -6,3 +6,9 @@ console.log('Recorrer un array con for')
 for (let i = 0; i < array.length; i++) {
     console.log(array[i]);
 } // El ciclo for es una estructura de control que te permite iterar sobre los elementos de un array de manera manual, controlando el índice y los límites de la iteración.
+
+console.log('Recorrer un array de forEach')
+array.forEach((elemento, index) => {
+    console.log(`${elemento} en la posición ${index}`)
+}) // forEach no te da ese nivel de control. No puedes usar break o continue para detener o saltar elementos. 
+   //Para detener forEach, necesitarías formas alternativas, como devolver false o utilizar try...catch, pero en general, no está diseñado para ese propósito.
