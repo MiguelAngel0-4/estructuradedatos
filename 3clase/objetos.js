b1f78ff24bc8f1a400d4ad32dcbb69e6105b8626
Miguel <migue2801224@gmail.com>
2024-10-30 23:38:09 -0500

    primer commit

---
+++ 3clase/objetos.js
@@ -0,0 +1,12 @@
+const usuario={
+    nombre: "carlos",
+    edad: 25,
+    trabaja: true,
+}
+//Imprimir a las propiedades de un objeto
+console.log(usuario)
+
+//Imprimir propiedades por separado
+console.log(usuario.nombre)
+console.log(usuario.edad)
+console.log(usuario.trabaja)
