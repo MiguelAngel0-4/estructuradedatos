d54e5ca6a22c70ebef5757c9e1361d58700115be
Miguel <migue2801224@gmail.com>
2024-10-30 23:27:31 -0500

    frutas definicion arreglo

---
+++ punto2.js
@@ -0,0 +1,11 @@
+//2) 
+// Crea un array llamado frutas que contenga las frutas: "manzana", "pera", "plátano", y "cereza".
+// *Encontrar la posición de "plátano" en el array.
+// *agregar un"mango" al final del array.
+// *agregar "fresa" al inicio del array.
+// *eliminar el primer elemento del array.
+// *eliminar el último elemento del array.
+// Al final, muestra en la consola el estado del array después de cada operación.
+
+//creamos el array
+let frutas = ["manzana", "pera", "plátano", "cereza"];
